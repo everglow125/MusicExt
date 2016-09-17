b72d4621e446131a72710ff203f558fed544180b
everglow125 <183900112@qq.com>
2016-09-18 07:50:45 +0800

    添加

--- MusicExt/MusicExt/Program.cs
+++ MusicExt/MusicExt/Program.cs
@@ -16,7 +16,7 @@ namespace MusicExt
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Stock());
         }
     }
 }
